3df6c6a8e6ac0aa85167f89f188fa85aaea390a0
harwinder2120-dotcom <harwinder2120@gmail.com>
2026-02-18 23:00:14 -0500

    Updated project with CSS and favicon

---
+++ Hello_World.js
@@ -0,0 +1,9 @@
+let greeting = "Hello World";
+console.log(greeting);
+
+let greeting2 = "Hello Again";
+console.log(greeting2);
+
+let sentence1 = "This is a sentence.";
+console.log(sentence1); // ← this is intentionally wrong for the assignment
+
